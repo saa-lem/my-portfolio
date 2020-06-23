a2c01ee2fb8c79bf5387d2b4c8b58b071603cbf8
Salem Owino <salemowino18@gmail.com>
2020-06-23 09:02:06 +0300

    Image path rendered

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -152,8 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/project_images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'project_images')
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
